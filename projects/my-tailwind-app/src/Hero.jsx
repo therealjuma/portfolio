3c43a369ddec89a05e4946ea5585a22fe8246e4d
therealjuma <jumadenis404@gmail.com>
2025-09-26 11:01:17 +0300

    Minor update

--- projects/my-tailwind-app/src/Hero.jsx
+++ projects/my-tailwind-app/src/Hero.jsx
@@ -3,7 +3,7 @@ import myProfileImage from './assets/photo.jpg';
 
 const Hero = () => {
     return (
-        <section class="bg-gray-100">
+        <section class="bg-gray-100 dark:bg-gray-800">
             <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
                 <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
                     <div class="max-w-lg">
